0d0570829472b5da482f5a2b49a23ee3f86b3fcd
Ahsan Ali (SP24-BSE-004) <ahsanalich358@gmail.com>
2026-05-07 08:32:53 +0500

    Add files via upload

---
+++ 2026_05_04_000001_create_user_registrations_table.php.php
@@ -0,0 +1,20 @@
+<?php
+// FILE: app/Models/UserRegistration.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserRegistration extends Model
+{
+    protected $table = 'userRegistration';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'cnic',
+        'telephone',
+        'comments',
+        'profile_picture',
+    ];
+}
